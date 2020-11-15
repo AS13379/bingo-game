83df3aea8f1285fd4f638c9a77480a2c2457abf8
Charlie894 <63108760+Charlie894@users.noreply.github.com>
2020-11-15 20:58:32 +0800

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,38 @@
+#bingo-game
+def check_bingo():
+    for i in range(5):
+        tmp=[]
+        for j in range(5):
+            tmp.append(empty[j][i])
+        if tmp.count('##') == 0:
+            return True
+    tmp=[]
+    for i in range(5):
+        tmp.append(empty[i][i])
+    if tmp.count('##') ==0:
+        return True
+    tmp=[]
+    for i in range(5):
+        tmp.append(empty[i][4-i])
+    if tmp.count('##') ==0:
+        return True
+    return False
+num_list=list(range(1,76))
+
+count=0
+while count < 35:
+    num=num_list[randint(0,len(num_list)-1)]
+    num_list.remove(num)
+    print('Pickup Number is',num)
+    for i in range(5):
+        for j in range(5):
+            if bingo[j][i]==num:
+                empty[j][i]=num
+    print_table()
+    if check_bingo():
+        print('BINGO!!!')
+        break
+    count += 1
+    time.sleep(1)
+else:
+    print('运气不好')
